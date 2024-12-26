032ee6f0f1adfea9c62d4176ea112fd3015fab58
Dev Karan <88235329+KaranCodeCraft@users.noreply.github.com>
2024-12-26 15:18:11 +0530

    server url

--- context/ApiState.js
+++ context/ApiState.js
@@ -2,31 +2,31 @@ import { useState } from "react";
 import ApiContext from "./ApiContext";
 import { Alert } from "react-native";
 
-
 const ApiState = (props) => {
-    const url = "http://192.168.1.100:5000/"
-    const [user, setUser] = useState("")
+  const url = "https://api.sikkimglobaltechnicaluniversity.co.in/";
+  // const url = "http://192.168.1.100:5000/";
+  const [user, setUser] = useState("");
 
-    async function verifyToken(token) {
-      try {
-        const response = await fetch(`${url}/api/aadhaar-login/verify-token`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ token })
-        });
-        const data = await response.json();
-       setUser(data.username);
-        
-        if(response.status == 200)return true;
+  async function verifyToken(token) {
+    try {
+      const response = await fetch(`${url}/api/aadhaar-login/verify-token`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token }),
+      });
+      const data = await response.json();
+      setUser(data.username);
 
-        return false
-      } catch (error) {
-        console.error("Error verifying token:", error);
-        return false;
-      }
+      if (response.status == 200) return true;
+
+      return false;
+    } catch (error) {
+      console.error("Error verifying token:", error);
+      return false;
     }
+  }
 
   return (
     <ApiContext.Provider
